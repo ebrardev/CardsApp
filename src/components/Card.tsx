0c6aaccac7a239843cb3a644810fc0eee0da3f91
ebrardev <16155052@mersin.edu.tr>
2024-05-02 17:11:54 +0300

    Add animation support to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,24 +1,53 @@
- import  Animated, {clamp, useAnimatedReaction, useDerivedValue, useSharedValue} from 'react-native-reanimated';
+ import  Animated, {clamp, useAnimatedReaction, useDerivedValue, useSharedValue, withTiming} from 'react-native-reanimated';
 import React, { useState } from 'react';
 import {Gesture,GestureDetector} from "react-native-gesture-handler"
+import { useWindowDimensions } from 'react-native';
 
 
 
 const Card =({card,index,scrollY,activeCardIndex}) =>{
   const [cardHeight, setCardHeight] = useState(0)
   const translateY = useSharedValue(0)
+  const {height:screenHeight} = useWindowDimensions()
 
-  useAnimatedReaction(()=>{
-    return scrollY.value
-},
+  useAnimatedReaction(()=>
+     scrollY.value
+,
 (current)=>{
     translateY.value= clamp(-current,-index*cardHeight,0)
   })
     // const translateY = useDerivedValue(()=>clamp(-scrollY.value,-index*cardHeight,0))
+   useAnimatedReaction(()=>activeCardIndex.value,
+   (current,pervious) => {
+      if (current === pervious) {
+        return
+      }
+      // no card selected 
+      if(activeCardIndex.value === null){
+        translateY.value=withTiming(clamp(-scrollY.value,-index*cardHeight,0))
+        
+      }else if(activeCardIndex.value === index) {
+             // this card becomes active
+             translateY.value=withTiming(-index*cardHeight)
+      
+      }
+      
+      else {
+        translateY.value=withTiming( -index*cardHeight*0.9+screenHeight*0.7)
+      }
 
+
+     // Another card is active
+
+   }
+   )
     const tap = Gesture.Tap().onEnd(()=>{
-    console.warn(index)
-    activeCardIndex.value = activeCardIndex.value === index ? null : index
+
+       if (activeCardIndex.value === null) {
+        activeCardIndex.value = index
+       } else {
+        activeCardIndex.value = null
+       }
     })
     return (
  <GestureDetector  gesture={tap} >
